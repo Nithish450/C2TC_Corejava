971ec4c611f9d9983665623431120b93f5dcb80a
NITHISH <NITHISH@DESKTOP-JQ3J754>
2023-07-27 16:00:59 +0530

    Inheritance

---
+++ Core_Java/src/com/cg/daytwo/inheitance/Family.java
@@ -0,0 +1,14 @@
+package com.cg.daytwo.inheitance;
+
+public class Family {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child c = new Child();
+		c.add();
+		c.sub();
+		c.div();
+
+	}
+
+}
---
+++ Core_Java/src/com/cg/daytwo/inheitance/GrandParent.java
@@ -0,0 +1,9 @@
+package com.cg.daytwo.inheitance;
+
+public class GrandParent {
+	int add() {
+		int a=20;
+		System.out.println(a+50);
+		return 0;
+	}
+}
